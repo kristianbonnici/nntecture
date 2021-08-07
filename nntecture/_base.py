b7c4992e08965c342942227668d01803fb92b202
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-08-07 14:38:56 +0200

    + docstring

--- nntecture/_base.py
+++ nntecture/_base.py
@@ -1,12 +1,30 @@
 """Main module."""
 
+# Author: Kristian Bonnici <kristiandaaniel@gmail.fi>
+
 import graphviz
 
 
 class DrawNN:
-    """ """
+    """
+    DrawNN is a class for Neural Network architecture visualization.
 
-    def __init__(self, layers, nn_type='ANN'):
+    Parameters
+    ----------
+    layers : list
+        Number of neurons for each layer. I.e. [2,4,3,1] is constructed as
+        2 Neurons in the input layer, 4 Neurons in the 1st hidden layer,
+        3 Neurons in the 2nd hidden layer, and 1 Neuron in the output layer.
+
+    nn_type : {'ANN', 'RNN'}, default='ANN'
+        This parameter specifies the type of the Neural Network.
+        - if `'ANN'`, a Artificial Neural Network (ANN) will be created;
+        - if `'RNN'`, a Recurrent Neural Network (RNN) will be created;
+    """
+
+    def __init__(self,
+                 layers,
+                 nn_type='ANN'):
         self.layers = layers
 
         # init neural network
@@ -27,7 +45,7 @@ class DrawNN:
 
     def draw(self,
              direction='LR',
-             linewidth=1,
+             linewidth=1.0,
              size='4,4!',
              fillcolor=None,
              node_labels=None,
@@ -38,34 +56,53 @@ class DrawNN:
              graph_fontcolor='#000000',
              fontname='helvetica'):
         """
+        Draw Neural Network architecture.
 
         Parameters
         ----------
-        direction : str
-             (Default value = 'LR')
-        linewidth : float
-             (Default value = 1)
-        size : str
-             (Default value = '4,4!')
-        fillcolor : str
-             (Default value = None)
-        node_labels : bool or str
-             (Default value = None)
-        graph_label : str
-             (Default value = None)
-        node_fontsize : float
-             (Default value = 10)
-        graph_fontsize : float
-             (Default value = 12)
-        node_fontcolor : str
-             (Default value = '#000000')
-        graph_fontcolor : str
-             (Default value = '#000000')
-        fontname : str
-             (Default value = 'helvetica')
+        direction : {'LR', 'RL', 'UD', 'DU'}, default='LR'
+            This parameter specifies the direction of the Neural Network.
+            - if `'LR'`, from left to right;
+            - if `'RL'`, from right to left;
+            - if `'UD'`, from up to down;
+            - if `'DU'`, from down to up;
+
+        linewidth : float, default=1.0, minimum=0.0
+            Specifies the width of the lines and curves.
+
+        size : str, default='4,4!'
+            Maximum width and height of drawing, in inches.
+            If size ends in an exclamation point "!", then size is taken to be the desired minimum size.
+
+        fillcolor : str, default=None
+            Color used to fill the background of a node.
+            - if None, fillcolor of "#D3D3D3" will be applied;
+
+        node_labels : True or str, default=True
+             Labels for the nodes.
+             - if True, nodes will have a layer number, and number of a node in the layer;
+
+        graph_label : str, default=None
+            Label for the graph.
+
+        node_fontsize : float, default=10, minimum=1.0
+            Font size, in points, used for node labels.
+
+        graph_fontsize : float, default=12, minimum=1.0
+            Font size, in points, used for graph label.
+
+        node_fontcolor : str, default='#000000'
+            Color used for node labels.
+
+        graph_fontcolor : str, default='#000000'
+            Color used for graph label.
+
+        fontname : str, default='helvetica'
+            Font used for text.
 
         Returns
         -------
+        display : :class:`~nntecture.DrawNN.graph_object`
 
         """
 
@@ -80,7 +117,7 @@ class DrawNN:
         self.graph_object.graph_attr["fontcolor"] = graph_fontcolor
 
         self.graph_object.node_attr["fixedsize"] = "false"
-        self.graph_object.edge_attr["style"] = "setlinewidth({})".format(linewidth)
+        self.graph_object.edge_attr["penwidth"] = str(linewidth)
         self.graph_object.node_attr["style"] = "filled"
         if fillcolor is not None:
             self.graph_object.node_attr["fillcolor"] = fillcolor
@@ -101,34 +138,46 @@ class DrawNN:
     def save(self,
              filename='nntecture',
              output_format='pdf',
+             dpi=96.0,
              size='4,4!',
              directory=None,
              cleanup=True,
              view=True):
         """
+        Save the current Neural Network architecture drawing.
 
         Parameters
         ----------
-        filename :
-             (Default value = 'nntecture')
-        output_format :
-             (Default value = 'pdf')
-        size :
-             (Default value = '4)
-        4!' :
+        filename : str, default='nntecture'
+            Name of the file to be saved.
 
-        directory :
-             (Default value = None)
-        cleanup :
-             (Default value = True)
-        view :
-             (Default value = True)
+        output_format : str, default='pdf'
+            Format of the file to be saved.
 
-        Returns
-        -------
+        dpi : float, default=96.0
+            Specifies the expected number of pixels per inch on a display device.
+            For bitmap output,
+            dpi guarantees that text rendering will be done more accurately, both in size and in placement.
+            For SVG output,
+            dpi guarantees the dimensions in the output correspond to the correct number of points or inches.
+
+        size : str, default='4,4!'
+            Maximum width and height of drawing, in inches.
+            If size ends in an exclamation point "!", then size is taken to be the desired minimum size.
+
+        directory : default=None
+            Directory where the file should be saved.
+            - if None, file is saved to current working directory (cwd);
+
+        cleanup : {True, False}, default=True
+            Delete the source file after successful rendering.
+
+        view : {True, False}, default=True
+            Open the rendered result with the default application.
 
         """
         self.graph_object.attr(size=size)
+        self.graph_object.graph_attr['dpi'] = str(dpi)
         self.graph_object.render(filename=filename,
                                  format=output_format,
                                  directory=directory,
