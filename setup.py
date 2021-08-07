f1a8044d82282c404d688ee027c05174e42a06e3
Kristian Bonnici <kristiandaaniel@gmail.com>
2021-08-07 16:02:17 +0200

    coverage

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
     ],
     description="NNtecture is a Python package for Neural Network architecture visualization.",
     install_requires=requirements,
